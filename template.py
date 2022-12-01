7b07d2e27b0affde4db917c433a94287f1f6b2df
WPBack <daniel.a.back@telia.com>
2022-12-01 19:14:46 +0100

    Added template from 2021

---
+++ template.py
@@ -0,0 +1,52 @@
+# Import modules
+import os
+from termcolor import colored
+
+# Input parser
+def input_parser(filename):
+    # Open the file
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    inputFile = open(os.path.join(__location__, filename), 'r')
+
+    # Process the file
+    input = inputFile.readlines()
+
+    # Close the file and return the result
+    inputFile.close()
+    return input
+
+# Puzzle 1
+def puzzle1(filename):
+    # Read file
+    input = input_parser(filename)
+
+    return 0
+
+# Puzzle 2
+def puzzle2(filename):
+    # Read file
+    input = input_parser(filename)
+
+    return 0
+
+# Run tests for puzzle 1
+puzzle1TestPass = puzzle1('example1') == 1
+if(puzzle1TestPass):
+    print(colored('Tests for puzzle 1 PASS', 'green'))
+else:
+    print(colored('Tests for puzzle 1 FAIL', 'red'))
+
+# Solve puzzle 1 if test passed
+if(puzzle1TestPass):
+    print('Solution for puzzle 1: ' + str(puzzle1('input')))
+
+# Run tests for puzzle 2
+puzzle2TestPass = puzzle2('example1') == 2
+if(puzzle2TestPass):
+    print(colored('Tests for puzzle 2 PASS', 'green'))
+else:
+    print(colored('Tests for puzzle 2 FAIL', 'red'))
+
+# Solve puzzle 2 if test passed
+if(puzzle2TestPass):
+    print('Solution for puzzle 2: ' + str(puzzle2('input')))
