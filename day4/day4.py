7e84592b1e91640c82d1ee997331b47e442d7dbf
WPBack <daniel.a.back@telia.com>
2022-12-04 11:19:16 +0100

    Solved day 4 part 1

--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,7 @@
 # Import modules
 import os
 from termcolor import colored
+import re
 
 # Input parser
 def input_parser(filename):
@@ -9,7 +10,8 @@ def input_parser(filename):
     inputFile = open(os.path.join(__location__, filename), 'r')
 
     # Process the file
-    lines = inputFile.read().splitlines()
+    #lines = [re.findall(r'\d+', line) for line in inputFile.read().splitlines()]
+    lines = [[int(char) for char in re.findall(r'\d+', line)] for line in inputFile.read().splitlines()]
 
     # Close the file and return the result
     inputFile.close()
@@ -19,8 +21,13 @@ def input_parser(filename):
 def puzzle1(filename):
     # Read file
     inputs = input_parser(filename)
+    
+    result = 0
+    for pair in inputs:
+        if (pair[0] <= pair[2] and pair[1] >= pair[3]) or (pair[2] <= pair[0] and pair[3] >= pair[1]):
+            result += 1
 
-    return 0
+    return result
 
 # Puzzle 2
 def puzzle2(filename):
@@ -31,7 +38,7 @@ def puzzle2(filename):
 
 # Run tests for puzzle 1
 puzzle1Result = puzzle1('example1')
-puzzle1TestPass = puzzle1Result == 1
+puzzle1TestPass = puzzle1Result == 2
 if(puzzle1TestPass):
     print(colored('Tests for puzzle 1 PASS', 'green'))
 else:
