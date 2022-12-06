f84d7b0c42ffd4e49953400e524ff90d5c2bb3ca
WPBack <daniel.a.back@telia.com>
2022-12-06 19:07:23 +0100

    Solved day 6 part 1

---
+++ day6/day6.py
@@ -0,0 +1,62 @@
+# Import modules
+import os
+from termcolor import colored
+
+# Input parser
+def input_parser(filename):
+    # Open the file
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    inputFile = open(os.path.join(__location__, filename), 'r')
+
+    # Process the file
+    line = inputFile.read()
+
+    # Close the file and return the result
+    inputFile.close()
+    return line
+
+# Puzzle 1
+def puzzle1(filename):
+    # Read file
+    inputs = input_parser(filename)
+
+    result = 4
+    while len(set(inputs[result-4:result])) != 4:
+        result += 1
+
+    return result
+
+# Puzzle 2
+def puzzle2(filename):
+    # Read file
+    inputs = input_parser(filename)
+
+    return 0
+
+# Run tests for puzzle 1
+puzzle1Result1 = puzzle1('example1')
+puzzle1Result2 = puzzle1('example2')
+puzzle1Result3 = puzzle1('example3')
+puzzle1Result4 = puzzle1('example4')
+puzzle1Result5 = puzzle1('example5')
+puzzle1TestPass = puzzle1Result1 == 7 and puzzle1Result2 == 5 and puzzle1Result3 == 6 and puzzle1Result4 == 10 and puzzle1Result5 == 11
+if(puzzle1TestPass):
+    print(colored('Tests for puzzle 1 PASS', 'green'))
+else:
+    print(colored(f'Tests for puzzle 1 FAIL, got {puzzle1Result1}, {puzzle1Result2}, {puzzle1Result3}, {puzzle1Result4}, {puzzle1Result5}', 'red'))
+
+# Solve puzzle 1 if test passed
+if(puzzle1TestPass):
+    print('Solution for puzzle 1: ' + str(puzzle1('input')))
+
+# Run tests for puzzle 2
+puzzle2Result = puzzle2('example1')
+puzzle2TestPass = puzzle2Result == 2
+if(puzzle2TestPass):
+    print(colored('Tests for puzzle 2 PASS', 'green'))
+else:
+    print(colored(f'Tests for puzzle 2 FAIL, got {puzzle2Result}', 'red'))
+
+# Solve puzzle 2 if test passed
+if(puzzle2TestPass):
+    print('Solution for puzzle 2: ' + str(puzzle2('input')))
